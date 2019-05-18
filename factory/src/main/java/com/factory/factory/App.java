0a1554b59dc4b58c1910199a57cf04c297bf2d6d
FernandoUnix <01fernando.ti@gmail.com>
2019-05-18 15:23:45 -0300

    inicio decorator

--- factory/src/main/java/com/factory/factory/App.java
+++ factory/src/main/java/com/factory/factory/App.java
@@ -3,6 +3,7 @@ package com.factory.factory;
 
 public class App {
 	public static void main(String[] args) {
+		
 		var veiculo = new FactoryConcreta().criarVeiculo(2, CondicaoMetereologica.SOL);
 		System.out.println(veiculo.getClass().getName());
 		veiculo.Imprimir();

0b18976d92580284804a8c0062c9141dbfd8e573
FernandoUnix <01fernando.ti@gmail.com>
2019-06-01 14:40:17 -0300

    observer

---
+++ observer/src/main/java/com/observer/observer/App.java
@@ -0,0 +1,13 @@
+package com.observer.observer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
